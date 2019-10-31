92bb644bd5a014e37c2debff189f2f9af4b39d35
seongheejin <ganghun7558@hanmail.net>
2019-10-31 15:29:07 +0900

    boj2839

--- 2839.cpp
+++ 2839.cpp
@@ -4,10 +4,15 @@
 #include <iostream>
 
 int boj2839() {
-
-
-
+    int N;
+    std::cin >> N;
+    if ((N%5)%3||(N%3)%5 != 0) {
+        std::cout << -1;
+        return 0;
+    }
+    int A = N/5;
+    int B = N%5/3;
+    std::cout << A + B;
 
     return 0;
 }
-
